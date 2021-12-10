3184351732f8607f6dc693d933e2a8f192c70dc5
Randa <randazraik@outlook.com>
2021-12-10 21:51:32 +0200

    Add more unit tests for ProfilesService

--- tests/Turing.Business.Tests/Services/ProfilesServiceTests.cs
+++ tests/Turing.Business.Tests/Services/ProfilesServiceTests.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using Optional;
 using Shouldly;
 using System;
 using System.Threading.Tasks;
 using Turing.Business.Services;
 using Turing.Core.Identity;
+using Turing.Core.Models;
 using Turing.Data.Entities;
 using Turing.Data.EntityFramework;
 using Xunit;
@@ -20,6 +23,9 @@ namespace Turing.Business.Tests.Services
         private readonly Mock<UserManager<User>> _userManagerMock;
         private readonly Mock<IJwtFactory> _jwtFactoryMock;
         private readonly Mock<IMapper> _mapperMock;
+
+        private readonly User _user1 = new User { Id = Guid.NewGuid().ToString(), UserName = "user1", Email = "user1@users.com" };
+        private readonly User _user2 = new User { Id = Guid.NewGuid().ToString(), UserName = "user2", Email = "user2@users.com" };
 
         public ProfilesServiceTests()
         {
@@ -43,70 +49,169 @@ namespace Turing.Business.Tests.Services
         public async Task Follow_ReturnsCorrectData_ByDefault()
         {
             // Arrange
-            var user1 = new User { Id = Guid.NewGuid().ToString(), UserName = "user1", Email = "user1@users.com" };
-            var user2 = new User { Id = Guid.NewGuid().ToString(), UserName = "user2", Email = "user2@users.com" };
+            await AddUserWithEmailAsync(_user1.Email, _user1);
+            await AddUserWithEmailAsync(_user2.Email, _user2);
 
-            AddUserWithEmail(user1.Email, user1);
-            AddUserWithEmail(user2.Email, user2);
-
-            _dbContext.FollowedUsers.Add(new FollowedUser
-            {
-                FollowerId = user1.Id,
-                FollowingId = user2.Id,
-            });
-            await _dbContext.SaveChangesAsync();
+            _mapperMock.Setup(mapper => mapper
+                .Map<UserProfileModel>(It.IsAny<User>()))
+                .Returns(new UserProfileModel { Id = _user2.Id });
 
             // Act
-            var followResp = await _profilesService.FollowAsync(user1.Id, user2.UserName);
+            var followResp = await _profilesService.FollowAsync(_user1.Id, _user2.UserName);
 
             // Assert
-            followResp.MatchSome(model => model.Id.ShouldBe(user2.Id));
+            followResp.MatchSome(model => model.Id.ShouldBe(_user2.Id));
+            followResp.MatchSome(model => model.Following.ShouldBe(true));
         }
 
         [Fact]
         public async Task Follow_ReturnsError_WhenFollowingAnAlreadyFollowedUser()
         {
             // Arrange
-            var user1 = new User { Id = Guid.NewGuid().ToString(), UserName = "user1", Email = "user1@users.com" };
-            var user2 = new User { Id = Guid.NewGuid().ToString(), UserName = "user2", Email = "user2@users.com" };
-
-            AddUserWithEmail(user1.Email, user1);
-            AddUserWithEmail(user2.Email, user2);
-
-            _dbContext.FollowedUsers.Add(new FollowedUser
-            {
-                FollowerId = user1.Id,
-                FollowingId = user2.Id,
-            });
-            await _dbContext.SaveChangesAsync();
+            await AddUserWithEmailAsync(_user1.Email, _user1);
+            await AddUserWithEmailAsync(_user2.Email, _user2);
+            await FollowUserAsync(_user1.Id, _user2.Id);
 
             // Act
-            var followResp = await _profilesService.FollowAsync(user1.Id, user2.UserName);
+            var result = await _profilesService.FollowAsync(_user1.Id, _user2.UserName);
 
             // Assert
-            followResp.MatchNone(error => error.Messages?[0].ShouldBe("You are already following this user"));
+            result.MatchNone(error => error.Messages?.Count.ShouldBeGreaterThan(0));
         }
 
         [Fact]
         public async Task Follow_ReturnsError_WhenUserFollowHimself()
         {
             // Arrange
-            var user1 = new User { Id = Guid.NewGuid().ToString(), UserName = "user1", Email = "user1@users.com" };
-
-            AddUserWithEmail(user1.Email, user1);
+            await AddUserWithEmailAsync(_user1.Email, _user1);
 
             // Act
-            var followResp = await _profilesService.FollowAsync(user1.Id, user1.UserName);
+            var result = await _profilesService.FollowAsync(_user1.Id, _user1.UserName);
 
             // Assert
-            followResp.MatchNone(error => error.Messages?[0].ShouldBe("A user cannot follow himself."));
+            result.MatchNone(error => error.Messages?.Count.ShouldBeGreaterThan(0));
         }
 
-        private void AddUserWithEmail(string email, User expected)
+        [Fact]
+        public async Task Unfollow_ReturnsCorrectData_ByDefault()
+        {
+            // Arrange
+            await AddUserWithEmailAsync(_user1.Email, _user1);
+            await AddUserWithEmailAsync(_user2.Email, _user2);
+            await FollowUserAsync(_user1.Id, _user2.Id);
+
+            _mapperMock.Setup(mapper => mapper
+                .Map<UserProfileModel>(It.IsAny<User>()))
+                .Returns(new UserProfileModel { Id = _user2.Id });
+
+            // Act
+            var result = await _profilesService.UnfollowAsync(_user1.Id, _user2.UserName);
+
+            // Assert
+            result.MatchSome(model => model.Id.ShouldBe(_user2.Id));
+            result.MatchSome(model => model.Following.ShouldBe(false));
+
+            _dbContext.FollowedUsers.ShouldNotContain(new FollowedUser
+            {
+                FollowerId = _user1.Id,
+                FollowingId = _user2.Id,
+            });
+        }
+
+        [Fact]
+        public async Task Unfollow_ReturnsError_WhenUnfollowingUserNotAlreadyFollowing()
+        {
+            // Arrange
+            await AddUserWithEmailAsync(_user1.Email, _user1);
+            await AddUserWithEmailAsync(_user2.Email, _user2);
+
+            // Act
+            var result = await _profilesService.UnfollowAsync(_user1.Id, _user2.UserName);
+
+            // Assert
+            result.MatchNone(error => error.Messages?.Count.ShouldBeGreaterThan(0));
+        }
+
+        [Fact]
+        public async Task Unfollow_ReturnsError_WhenUserUnfollowHimself()
+        {
+            // Arrange
+            await AddUserWithEmailAsync(_user1.Email, _user1);
+
+            // Act
+            var result = await _profilesService.UnfollowAsync(_user1.Id, _user1.UserName);
+
+            // Assert
+            result.MatchNone(error => error.Messages?.Count.ShouldBeGreaterThan(0));
+        }
+
+        [Fact]
+        public async Task ViewProfile_ReturnsFollowedUserProfile_ByDefault()
+        {
+            // Arrange
+            await AddUserWithEmailAsync(_user1.Email, _user1);
+            await AddUserWithEmailAsync(_user2.Email, _user2);
+
+            _mapperMock.Setup(mapper => mapper
+                .Map<UserProfileModel>(It.IsAny<User>()))
+                .Returns(new UserProfileModel { Id = _user2.Id });
+
+            // Act
+            var result = await _profilesService.ViewProfileAsync(_user1.Id.SomeNotNull(), _user2.UserName);
+
+            // Assert
+            result.MatchSome(model => model.Id.ShouldBe(_user2.Id));
+            result.MatchSome(model => model.Following.ShouldBe(false));
+        }
+
+        [Fact]
+        public async Task ViewProfile_ReturnsUnfollowedUserProfile_ByDefault()
+        {
+            // Arrange
+            await AddUserWithEmailAsync(_user1.Email, _user1);
+            await AddUserWithEmailAsync(_user2.Email, _user2);
+            await FollowUserAsync(_user1.Id, _user2.Id);
+
+            _mapperMock.Setup(mapper => mapper
+                .Map<UserProfileModel>(It.IsAny<User>()))
+                .Returns(new UserProfileModel { Id = _user2.Id });
+
+            // Act
+            var result = await _profilesService.ViewProfileAsync(_user1.Id.SomeNotNull(), _user2.UserName);
+
+            // Assert
+            result.MatchSome(model => model.Id.ShouldBe(_user2.Id));
+            result.MatchSome(model => model.Following.ShouldBe(true));
+        }
+
+        private async Task FollowUserAsync(string userId1, string userId2)
+        {
+            var fo = new FollowedUser
+            {
+                FollowerId = userId1,
+                FollowingId = userId2,
+            };
+
+            _dbContext.FollowedUsers.Add(fo);
+            await _dbContext.SaveChangesAsync();
+            _dbContext.Entry(fo).State = EntityState.Detached;
+        }
+
+        private async Task UnfollowUserAsync(string userId1, string userId2)
+        {
+            _dbContext.FollowedUsers.Remove(new FollowedUser
+            {
+                FollowerId = userId1,
+                FollowingId = userId2,
+            });
+            await _dbContext.SaveChangesAsync();
+        }
+
+        private async Task AddUserWithEmailAsync(string email, User expected)
         {
             expected.Email = email;
             _dbContext.Users.Add(expected);
-            _dbContext.SaveChanges();
+            await _dbContext.SaveChangesAsync();
         }
     }
 }
