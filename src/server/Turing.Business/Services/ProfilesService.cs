813324c7f1bceb71b677ed7712adfa2132fe248d
Randa <randazraik@outlook.com>
2021-12-10 22:41:20 +0200

    Remove the solution

--- src/server/Turing.Business/Services/ProfilesService.cs
+++ src/server/Turing.Business/Services/ProfilesService.cs
@@ -1,13 +1,10 @@
 ﻿using AutoMapper;
+using Optional;
+using System.Threading.Tasks;
 using Turing.Core;
 using Turing.Core.Models;
 using Turing.Core.Services;
-using Turing.Data.Entities;
 using Turing.Data.EntityFramework;
-using Optional;
-using Optional.Async;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Turing.Business.Services
 {
@@ -19,62 +16,27 @@ namespace Turing.Business.Services
             IMapper mapper)
             : base(dbContext)
         {
-            UsersService = usersService;
-            Mapper = mapper;
         }
 
-        protected IMapper Mapper { get; }
-        protected IUsersService UsersService { get; }
+        /// <inheritdoc />
+        public Task<Option<UserProfileModel, Error>> FollowAsync(string followerId, string userToFollowUsername)
+        {
+            // Write your solution here.
+            return null;
+        }
 
-        public Task<Option<UserProfileModel, Error>> FollowAsync(string followerId, string userToFollowUsername) =>
-            GetUserByIdOrError(followerId).FlatMapAsync(user =>
-            GetUserByNameOrError(userToFollowUsername)
-                .FilterAsync(async u => u.Id != followerId, "A user cannot follow himself.")
-                .FilterAsync(async u => user.Following.All(fu => fu.FollowingId != u.Id), "You are already following this user")
-                .FlatMapAsync(async userToFollow =>
-                {
-                    DbContext.FollowedUsers.Add(new FollowedUser
-                    {
-                        FollowerId = followerId,
-                        FollowingId = userToFollow.Id
-                    });
+        /// <inheritdoc />
+        public Task<Option<UserProfileModel, Error>> UnfollowAsync(string followerId, string userToUnfollowUsername)
+        {
+            // Write your solution here.
+            return null;
+        }
 
-                    await DbContext.SaveChangesAsync();
-
-                    return await ViewProfileAsync(followerId.Some(), userToFollow.UserName);
-                }));
-
-        public Task<Option<UserProfileModel, Error>> UnfollowAsync(string followerId, string userToUnfollowUsername) =>
-            GetUserByIdOrError(followerId).FlatMapAsync(user =>
-            GetUserByNameOrError(userToUnfollowUsername)
-                .FilterAsync(async u => u.Id != followerId, "A user cannot unfollow himself.")
-                .FilterAsync(async u => user.Following.Any(fu => fu.FollowingId == u.Id), "You cannot unfollow users that you aren't following.")
-                .FlatMapAsync(async userToUnfollow =>
-                {
-                    var relationToRemove = user.Following.First(f => f.FollowingId == userToUnfollow.Id);
-
-                    DbContext.FollowedUsers.Remove(relationToRemove);
-
-                    await DbContext.SaveChangesAsync();
-
-                    return await ViewProfileAsync(followerId.Some(), userToUnfollow.UserName);
-                }));
-
-        public Task<Option<UserProfileModel, Error>> ViewProfileAsync(Option<string> viewingUserId, string profileUsername) =>
-            GetUserByNameOrError(profileUsername)
-                .MapAsync(async userToView =>
-                {
-                    var profile = Mapper.Map<UserProfileModel>(userToView);
-
-                    (await GetUserById(viewingUserId)).MatchSome(viewingUser =>
-                    {
-                        // If the user that is viewing the profile has the profile owner in his followers
-                        profile.Following = viewingUser
-                            .Following?
-                            .Any(f => f.FollowingId == userToView.Id) ?? false;
-                    });
-
-                    return profile;
-                });
+        /// <inheritdoc />
+        public Task<Option<UserProfileModel, Error>> ViewProfileAsync(Option<string> viewingUserId, string profileUsername)
+        {
+            // Write your solution here.
+            return null;
+        }
     }
 }
